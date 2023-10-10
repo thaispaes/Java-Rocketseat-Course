1decc684d1f24c10dd0ab4025ab7bba5a6e79ff7
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-10-10 14:50:25 -0300

    Feat: commit initial

---
+++ src/main/java/br/com/rocketseat/todolist/ProjetoTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //Annotations (Ela é uma função)
+public class ProjetoTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rocketseat/todolist/ProjetoTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
